d486f6b8cfd4b093d0957638e7f63bc907c5e7c9
rnjacky777 <rnjacky777@gmail.com>
2025-08-07 21:36:39 +0800

    Add github action

--- src/api/base.js
+++ src/api/base.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 // 創建一個 axios 實例
 const api = axios.create({
-  baseURL: 'http://localhost:8000/bo_api', // 你的後端 API 地址
+  baseURL: 'https://amon777.ddns.net/bo_api',
+  // baseURL: 'http://localhost:8000/bo_api', // 你的後端 API 地址
   headers: {
     'Content-Type': 'application/json',
   },
